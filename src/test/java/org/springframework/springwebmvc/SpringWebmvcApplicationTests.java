9a52835447cb8df7cb03fdba13bc9c2344a10048
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2023-03-31 11:07:01 -0300

    Subindo Projeto treino Spring

---
+++ src/test/java/org/springframework/springwebmvc/SpringWebmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.springwebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
